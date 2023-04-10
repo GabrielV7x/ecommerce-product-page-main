dd9dd303eada9b45232ec288c2b75a73ede2f911
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-04-10 19:44:44 +0400

    JS cart popop done

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ const menuAnimation = (li) => {
 };
 
 lis.forEach(menuAnimation);
+
+// Cart popup animation
+const cart = document.querySelector(".cart");
+const cartPopup = document.querySelector(".cart_popup");
+
+const cartPopupShow = (cart, cartPopup) => {
+  cart.addEventListener("click", function() {
+    console.log("test1");
+    if (cartPopup.style.display === "block") {
+      console.log("test2");
+      cartPopup.style.display = "none"
+    }
+    else {
+      console.log("test3");
+      cartPopup.style.display = "block"
+    }
+  });
+
+};
+
+cartPopupShow(cart,cartPopup);
