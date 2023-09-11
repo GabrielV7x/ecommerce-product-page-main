cc2fca93456eca973da166d07af29a5e7e0dfc6a
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-09-11 20:03:40 +0400

    modal - update

--- js/lightbox.js
+++ js/lightbox.js
@@ -16,25 +16,26 @@ function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
 
-  //  change the main img according to the thumbnails selected
+
+//  change the main img according to the thumbnails selected
+const thumbnailsLBox = [...document.querySelectorAll("#myModal .thumbnails img[src]")];
+
+function becomeMainImg(img) {
   const mainImgLBox = document.querySelector("#myModal .main_image img[src]");
 
-  const thumbnailsLBox = [...document.querySelectorAll("#myModal .thumbnails img[src]")];
-
-  function becomeMainImg(img) {
-    img.addEventListener("click", function() {
-      thumbnailsLBox.forEach(function(thumbnail) { // remove the mainImg class from all the thumbnails
-        thumbnail.parentNode.classList.remove("mainImg");
-        thumbnail.style.opacity = "1";
-      })
-
-      const imgAttrValue = img.getAttribute("src"); // get the src attribute value of the thumbnail
-      const bigImgPath = imgAttrValue.replace("-thumbnail", "");
-      const mainImgAttrValue = mainImgLBox.setAttribute("src", bigImgPath)
-
-      // img.style.opacity = "0.3";
-      img.parentNode.classList.add("mainImg"); // add the mainImg class to the thumbnail clicked
+  img.addEventListener("click", function() {
+    thumbnailsLBox.forEach(function(thumbnail) { // remove the mainImg class from all the thumbnails
+      thumbnail.parentNode.classList.remove("mainImg");
+      thumbnail.style.opacity = "1";
     })
-  }
 
-  thumbnailsLBox.forEach(becomeMainImg)
+    const imgAttrValue = img.getAttribute("src"); // get the src attribute value of the thumbnail
+    const bigImgPath = imgAttrValue.replace("-thumbnail", "");
+    const mainImgAttrValue = mainImgLBox.setAttribute("src", bigImgPath)
+
+    // img.style.opacity = "0.3";
+    img.parentNode.classList.add("mainImg"); // add the mainImg class to the thumbnail clicked
+  })
+}
+
+thumbnailsLBox.forEach(becomeMainImg)
